d6711ab6dae76d099178b4c8a41eefb6564ff946
Daniel-derf <danielpardinho321@gmail.com>
2025-03-31 23:30:07 -0300

    feat: accordion component replicated

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@ import "./App.css";
 // components
 import Accordion from "./components/Accordion";
 
+const CONTENT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nisl arcu, cursus scelerisque lorem in, pulvinar tincidunt erat. Duis varius commodo varius. Ut interdum lectus ut metus tempus viverra. Quisque quis luctus magna, sit amet tincidunt urna. Aenean eget sollicitudin mi. Praesent ultricies maximus lectus, et efficitur nisl tempor eget. Nullam at massa erat. Fusce semper vitae nisi sit amet egestas. Fusce sed eros nunc. Donec ut ante tempus, convallis eros vitae, commodo lectus. In molestie ipsum nec bibendum hendrerit. Nunc eleifend diam vehicula scelerisque malesuada. Aliquam erat volutpat. Donec vitae purus diam. Sed sed lacinia tellus.
+
+Pellentesque venenatis, ante volutpat sodales molestie, velit metus volutpat velit, nec hendrerit risus metus vitae ante. Sed eleifend eros in erat cursus accumsan. Proin vel commodo metus. Vestibulum cursus pretium viverra. Quisque eu sagittis est. Aenean tempor felis leo, sit amet mollis quam dignissim ac. Pellentesque lacinia enim et varius placerat.`;
+
 function App() {
   return (
     <>
-      <Accordion />
+      <Accordion title="DETAILS" content={CONTENT} />
+      <Accordion title="ABOUT US" content={CONTENT} />
+      <Accordion title="CONTACT" content={CONTENT} />
+      <Accordion title="END" content={CONTENT} />
     </>
   );
 }
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -14,23 +14,16 @@ function useAccordion() {
   return { isOpen, changeVisibility, className };
 }
 
-function Accordion() {
+function Accordion({ title, content }) {
   const { isOpen, changeVisibility, className } = useAccordion();
 
   return (
     <>
       <h2 className="section" onClick={changeVisibility}>
-        DETAILS {!isOpen ? <>↓</> : <>↑</>}
+        {title} {!isOpen ? <>↓</> : <>↑</>}
       </h2>
       <div className={className}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam tellus, ultrices quis lorem sit amet, laoreet
-          interdum nulla. Vivamus velit mauris, egestas at felis ac, rutrum semper dolor. Pellentesque iaculis diam
-          nibh, nec egestas tortor venenatis vitae. Quisque tellus urna, ultrices non blandit at, laoreet lacinia leo.
-          Aliquam aliquet nisi vehicula est aliquet pulvinar bibendum eget magna. Pellentesque et quam justo. Duis at
-          felis ultricies, blandit massa iaculis, tincidunt neque. Proin non nunc vitae dui faucibus rutrum ac nec dui.
-          Mauris vulputate sit amet ante vel feugiat. Donec sit amet fringilla diam, in euismod turpis.
-        </p>
+        <p>{content}</p>
       </div>
     </>
   );
