d2e8b36895bd682774089da844c83a32e55dd49a
Daniel-derf <danielpardinho321@gmail.com>
2025-03-31 22:47:05 -0300

    accordion initial setup

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>DETAILS ↓ ↑</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam tellus, ultrices quis lorem sit amet, laoreet
+        interdum nulla. Vivamus velit mauris, egestas at felis ac, rutrum semper dolor. Pellentesque iaculis diam nibh,
+        nec egestas tortor venenatis vitae. Quisque tellus urna, ultrices non blandit at, laoreet lacinia leo. Aliquam
+        aliquet nisi vehicula est aliquet pulvinar bibendum eget magna. Pellentesque et quam justo. Duis at felis
+        ultricies, blandit massa iaculis, tincidunt neque. Proin non nunc vitae dui faucibus rutrum ac nec dui. Mauris
+        vulputate sit amet ante vel feugiat. Donec sit amet fringilla diam, in euismod turpis.
+      </p>
+      <h2>ABOUT US ↓ ↑</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam tellus, ultrices quis lorem sit amet, laoreet
+        interdum nulla. Vivamus velit mauris, egestas at felis ac, rutrum semper dolor. Pellentesque iaculis diam nibh,
+        nec egestas tortor venenatis vitae. Quisque tellus urna, ultrices non blandit at, laoreet lacinia leo. Aliquam
+        aliquet nisi vehicula est aliquet pulvinar bibendum eget magna. Pellentesque et quam justo. Duis at felis
+        ultricies, blandit massa iaculis, tincidunt neque. Proin non nunc vitae dui faucibus rutrum ac nec dui. Mauris
+        vulputate sit amet ante vel feugiat. Donec sit amet fringilla diam, in euismod turpis.
+      </p>
+      <h2>CONTACT ↓ ↑</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam tellus, ultrices quis lorem sit amet, laoreet
+        interdum nulla. Vivamus velit mauris, egestas at felis ac, rutrum semper dolor. Pellentesque iaculis diam nibh,
+        nec egestas tortor venenatis vitae. Quisque tellus urna, ultrices non blandit at, laoreet lacinia leo. Aliquam
+        aliquet nisi vehicula est aliquet pulvinar bibendum eget magna. Pellentesque et quam justo. Duis at felis
+        ultricies, blandit massa iaculis, tincidunt neque. Proin non nunc vitae dui faucibus rutrum ac nec dui. Mauris
+        vulputate sit amet ante vel feugiat. Donec sit amet fringilla diam, in euismod turpis.
+      </p>
+    </>
+  );
+}
+
+export default App;
