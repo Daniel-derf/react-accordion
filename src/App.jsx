6c45fd53f967bfb27decaf0d8f11a9d815a2a8a3
Daniel-derf <danielpardinho321@gmail.com>
2025-04-01 23:44:30 -0300

    refactor: useHandleAccordions hook

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Accordion from "./components/Accordion";
 
 const CONTENT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nisl arcu, cursus scelerisque lorem in, pulvinar tincidunt erat. Duis varius commodo varius. Ut interdum lectus ut metus tempus viverra. Quisque quis luctus magna, sit amet tincidunt urna. Aenean eget sollicitudin mi. Praesent ultricies maximus lectus, et efficitur nisl tempor eget. Nullam at massa erat. Fusce semper vitae nisi sit amet egestas. Fusce sed eros nunc. Donec ut ante tempus, convallis eros vitae, commodo lectus. In molestie ipsum nec bibendum hendrerit. Nunc eleifend diam vehicula scelerisque malesuada. Aliquam erat volutpat. Donec vitae purus diam. Sed sed lacinia tellus. Pellentesque venenatis, ante volutpat sodales molestie, velit metus volutpat velit, nec hendrerit risus metus vitae ante. Sed eleifend eros in erat cursus accumsan. Proin vel commodo metus. Vestibulum cursus pretium viverra. Quisque eu sagittis est. Aenean tempor felis leo, sit amet mollis quam dignissim ac. Pellentesque lacinia enim et varius placerat.`;
 
-function App() {
+function useHandleAccordions() {
   const [displayDetails, setDisplayDetails] = useState(false);
   const [displayAbout, setDisplayAbout] = useState(false);
   const [displayContact, setDisplayContact] = useState(false);
@@ -30,6 +30,32 @@ function App() {
       }
     }
   }
+
+  return {
+    displayDetails,
+    setDisplayDetails,
+    displayAbout,
+    setDisplayAbout,
+    displayContact,
+    setDisplayContact,
+    displayEnd,
+    setDisplayEnd,
+    closeAll,
+  };
+}
+
+function App() {
+  const {
+    displayDetails,
+    displayAbout,
+    displayContact,
+    displayEnd,
+    setDisplayDetails,
+    setDisplayAbout,
+    setDisplayContact,
+    setDisplayEnd,
+    closeAll,
+  } = useHandleAccordions();
 
   return (
     <>
